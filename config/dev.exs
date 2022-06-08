86817c48bd39441dc5d3d2ebb482705e70ae08f9
jer <jer@moo>
2022-06-08 15:35:54 +0000

    feat: add tailwindcss

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,8 @@ config :mellifera, MelliferaWeb.Endpoint,
   secret_key_base: "y5QTm+x1RXGuSd4/ebZkztj4xDc01Qej3+xpmq6nY+EiwGvLMtItiOsQxbfnVxCn",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
