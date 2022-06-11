40847cd5cdd64a9874a3c421f9fc552f21aa2d99
jer <jer@moo>
2022-06-11 04:44:27 +0000

    feat(accounts): init accounts

---
+++ lib/mellifera/accounts/user.ex
@@ -0,0 +1,23 @@
+defmodule Mellifera.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "users" do
+    field :name, :string
+    field :email, :string
+    field :phone, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:name, :email, :phone])
+    |> validate_required([:name, :email, :phone])
+    |> unique_constraint(:email)
+    |> unique_constraint(:phone)
+  end
+end
---
+++ priv/repo/migrations/20220611042841_create_users.exs
@@ -0,0 +1,17 @@
+defmodule Mellifera.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :name, :string
+      add :email, :string
+      add :phone, :string
+
+      timestamps()
+    end
+
+    create unique_index(:users, [:email])
+    create unique_index(:users, [:phone])
+  end
+end
